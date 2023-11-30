d2912a3fb6caab89172ba7b12b3a63029a8034ff
Arafat <mdarafathosainchowdhory@gmail.com>
2023-12-01 02:50:06 +0600

    Positional argument

---
+++ positional_argument.py
@@ -0,0 +1,6 @@
+def user_info(fname, lname):
+    print("users first name is {}".format(fname))
+    print("users last name is {}".format(lname))
+    print("users full name is {} {}".format(fname, lname))
+user_info("Alvee", "Patuari")
+user_info("Arafat", "Chowdhory")
