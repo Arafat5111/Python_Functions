cf575a1fc68a97a86d1a2846490744efff196dce
Arafat <mdarafathosainchowdhory@gmail.com>
2023-12-01 18:08:19 +0600

    Time

---
+++ Time.py
@@ -0,0 +1,13 @@
+import time
+print(time.time())
+
+seconds = 1701432103.5767438
+
+print(time.ctime(seconds))
+
+
+# print("I am in new line")
+# time.sleep(5) # it will delly next line for 5 seconds
+# print("I am in another line")
+
+print(time.localtime(seconds))
