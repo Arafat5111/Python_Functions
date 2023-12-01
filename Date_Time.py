1ef51d20854786c7faba6204efd05b24f5c6ebab
Arafat <mdarafathosainchowdhory@gmail.com>
2023-12-01 18:20:30 +0600

    Date Time

--- Date_Time.py
+++ Date_Time.py
@@ -1,2 +1,12 @@
 import datetime
 print(datetime.datetime.now())
+
+print(datetime.datetime.utcnow())
+
+print(datetime.date.today())
+
+random_date = datetime.date.fromtimestamp(12345678)
+print(type(random_date))
+print(random_date.month)
+print(random_date.year)
+print(random_date.day)
